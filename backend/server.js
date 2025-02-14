37c1bea1b8d18487b16d3be45bc55a8434440596
nknandakuamr <tec.thunder2022nk@gmail.com>
2025-02-14 22:43:23 +0530

    Data base setup with neon and arcjet setup

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 //Routes
 import productRoutes from "./routes/productRoutes.js";
+import { sql } from "./config/db.js";
 //
 dotenv.config();
 const app = express();
@@ -16,6 +17,27 @@ app.use(helmet()); // helmet is a security middleware that helps you protect you
 app.use(morgan("dev")); // log the requests
 app.use("/api/products",productRoutes);
 
-app.listen(PORT, () => {
-	console.log(`server running on ${PORT} `);
+const initDB = async()=>{
+try {
+	await sql`
+	  CREATE TABLE IF NOT EXISTS products(
+	   id SERIAL PRIMARY KEY ,
+	   name VARCHAR(255) NOT NULL , 
+	   image VARCHAR(255) NOT NULL ,
+	   price DECIMAL(10,2) NOT NULL ,
+	   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+	  );
+	`
+	console.log("Database initialized successfully");
+	
+} catch (error) {
+	console.log("error",error);
+	
+}
+}
+
+initDB().then(()=>{
+	app.listen(PORT, () => {
+		console.log(`server running on ${PORT} `);
+	});
 });
